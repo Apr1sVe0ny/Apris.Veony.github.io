32c3c04a795cf604d52d4a4ec253a9b2d029cdb5
Arseny Gorokhoff <70861765+Apr1sVe0ny@users.noreply.github.com>
2020-09-19 21:21:12 +0400

    Add files via upload

---
+++ project3/slider/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('.imglink').slick({
+        arrows: false,
+        vertical: true,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        rows: 1,
+        slidesPerShow: 1,
+        draggable: false,
+
+    });
+
+    $('.right_image').slick({
+        arrows: false,
+        vertical: true,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        rows: 1,
+        slidesPerShow: 1,
+        draggable: false,
+    });
+});
