d205e4b956fe909457240a757f51adfb8cff00b9
Arseny Gorokhoff <70861765+Apr1sVe0ny@users.noreply.github.com>
2020-09-19 22:05:01 +0400

    Add files via upload

---
+++ docs/slider/script2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.con2tainer').slick({
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        rows: 1,
+        slidesPerShow: 1,
+        draggable: false,
+    });
+});
